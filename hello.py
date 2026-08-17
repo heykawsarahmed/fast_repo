8ab0ed705f9633058a4bdf35e2b5a5b287c4d1eb
heykawsarahmed <heykawsarahmed@gmail.com>
2026-08-17 14:53:11 +0600

    first commit

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print("hello World")
+
+print("hello everyone")
+a = int(input("enter your number"))
+
+print(f"your Binary number is {bin(a)}")
+print(f"your Binary number is {oct(a)}")
+print(f"your Binary number is {hex(a)}")
