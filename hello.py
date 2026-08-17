578fed58207eccdb1679921be1efee777afaecca
heykawsarahmed <heykawsarahmed@gmail.com>
2026-08-17 16:28:55 +0600

    first commit

--- hello.py
+++ hello.py
@@ -6,3 +6,11 @@ a = int(input("enter your number"))
 print(f"your Binary number is {bin(a)}")
 print(f"your Binary number is {oct(a)}")
 print(f"your Binary number is {hex(a)}")
+
+
+b = {
+    "name " : "johan",  
+    "age" : 25,
+    "gendar" : "male"
+}
+print(b)
