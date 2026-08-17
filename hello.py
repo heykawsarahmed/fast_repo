9ea13830343930c4d381b590f37a4a134ab42367
heykawsarahmed <heykawsarahmed@gmail.com>
2026-08-17 15:13:00 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 print("hello World")
 
-print("hello everyone")
+
 a = int(input("enter your number"))
 
 print(f"your Binary number is {bin(a)}")
 print(f"your Binary number is {oct(a)}")
 print(f"your Binary number is {hex(a)}")
 
-# abc
+print("hello everyone")
